ccc825a7154d03ffb464e3bffef69e4b1504ff4e
Oliver Mota <a7529@alunos.ipca.pt>
2017-03-25 22:09:12 +0000

    Função genérica criada e adicionada no main.c

--- main.c
+++ main.c
@@ -9,6 +9,82 @@
 #define MAX3(a,b,c) (a>b?(a>c?a:c):(b>c?b:c))
 #define MIN3(a,b,c) (a<b?(a<c?a:c):(b<c?b:c))
 
+//hue:0-360; saturation:0-100: value:0-360
+int rgb_to_hsv_by_color(IVC *src, IVC *dst, int hmin, int hmax, int smin, int smax, int vmin, int vmax)
+{
+	unsigned char *data_src = (unsigned char *)src->data;
+	int width = src->width;
+	int height = src->height;
+	int bytesperline = src->width * src->channels;
+	int channels = src->channels;
+	unsigned char *data_dst = (unsigned char *)dst->data;
+	
+	int x, y;
+	int long pos;
+	float rf, gf, bf;
+	float hue, saturation, value;
+	float max, min;
+
+
+	//verificação de erros
+	if( src->width <= 0 || src->height <= 0 || src->data == NULL) return 0;
+	if( dst->width != src->width || dst->height != src->height) return 0;
+	if(src->channels !=3 || dst->channels != 3) return 0;
+
+	for(x=0; x< width; x++)
+	{
+		for(y=0; y< height; y++)
+		{
+			pos = y*bytesperline + x*channels;
+			
+			rf = (float)data_src[pos];
+			gf = (float)data_src[pos+1];
+			bf = (float)data_src[pos+2];
+			
+			max = MAX3(rf,gf,bf);
+			min = MIN3(rf,gf,bf);
+
+			value = max;
+			if(value==0.0){ saturation=0.0; hue=0.0; }
+			else
+			{ 
+				saturation = ((value-min)/value)*255.0f;
+				if(saturation == 0.0) { hue=0.0; } 
+				else
+				{
+					if((max==rf)&&(gf>=bf)) hue = 60.0f * (gf-bf)/(max-min);
+					else if((max==rf)&&(gf<bf)) hue = 360.0f + 60.0f * (gf-bf)/(max-min);
+					else if(max==gf) hue = 120.0f + 60.0f * (bf-rf)/(max-min);
+					else hue = 240.0f + 60.0f * (rf-gf)/(max-min); //max == b
+				}
+				
+			}
+							
+			data_dst[pos]=0;	
+			data_dst[pos+1]=0;	
+			data_dst[pos+2]=0;	
+
+			if(hmin < hmax)
+			{
+				//segmentar apenas os pixeis			
+				if((hue >= hmin && hue <= hmax) && (saturation*100/255 >= smin && saturation*100/255 <= smax) && (value*100/255 >=vmin && value*100/255 <= vmax))
+				{
+					data_dst[pos]=255; data_dst[pos+1]=255; data_dst[pos+2] =255;
+				}
+			}
+			else
+			{
+				if((hue >= hmin || hue <= hmax) && (saturation*100/255 >= smin && saturation*100/255 <= smax) && (value*100/255 >=vmin && value*100/255 <= vmax))
+				{
+					data_dst[pos]=255; data_dst[pos+1]=255; data_dst[pos+2] =255;
+				}
+			}
+			
+		}
+	}
+	return 1;
+}
+
 int vc_rgb_to_hsv_b(IVC *src, IVC *dst)
 {
 	unsigned char *data_src = (unsigned char *)src->data;
@@ -218,7 +294,9 @@ int compare_area(const void *a, const void *b) {
 
 int color_segmentation(IVC *src) {
   IVC *dst = vc_image_new(src->width, src->height,src->channels,src->levels);
-  vc_rgb_to_hsv_r(src,dst);
+	//sinais vernelhos
+	rgb_to_hsv_by_color(src,dst,350,10,10,100,10,100);
+  //vc_rgb_to_hsv_r(src,dst);
 	//vc_rgb_to_hsv_b(src,dst);
   if (!vc_write_image_info("out/red.ppm", dst)) {
     error("process_file2: vc_write_image_info failed\n");
